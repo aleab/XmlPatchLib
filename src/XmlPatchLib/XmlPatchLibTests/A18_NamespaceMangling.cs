b0a767a9afa923dedbe8a614662bd20c89f1cfbd
aleab <11708634+aleab@users.noreply.github.com>
2023-09-22 22:15:05 +0200

    Namespace mangling passes tests

--- src/XmlPatchLib/XmlPatchLibTests/A18_NamespaceMangling.cs
+++ src/XmlPatchLib/XmlPatchLibTests/A18_NamespaceMangling.cs
@@ -19,28 +19,45 @@ namespace XmlPatchLibTests
             var doc = Shared.GetTestSampleWithNamespaces();
             var diff = XDocument.Load(@"TestData\A18_NamespaceMangling\NamespaceMangling.xml");
 
-            var nsResolver = new XmlNamespaceManager(new NameTable());
-            foreach (var ns in doc.Root!.GetNamespaceMap())
-            {
-                nsResolver.AddNamespace(string.IsNullOrWhiteSpace(ns.Key) ? "default" : ns.Key, ns.Value);
-            }
+            var nsResolver = doc.GetNamespaceResolver("default");
 
             Shared.Patcher.PatchXml(doc, diff);
 
-            var addedNode = doc.XPathSelectElement("//default:original")!.LastNode!;
+            var addedNode = doc.XPathSelectElement("//default:original", nsResolver)!.LastNode!;
+            Assert.AreEqual(XmlNodeType.Element, addedNode.NodeType);
+            Shared.TestWith((XElement)addedNode, elem =>
+            {
+                // <shouldbex:elem>  ==  <x:elem>
+                Assert.AreEqual("elem", elem.Name.LocalName);
+                Assert.AreEqual("http://schemas.microsoft.com/winfx/2006/xaml", elem.Name.Namespace.NamespaceName);
+                Assert.AreSame(elem, doc.XPathSelectElement("//x:elem", nsResolver));
+
+                Shared.TestWith((XElement)elem.FirstNode!, child =>
+                {
+                    // <empty:child>  ==  <child>
+                    Assert.AreEqual("child", child.Name.LocalName);
+                    Assert.AreEqual("http://www.w3.org/2001/XMLSchema", child.Name.Namespace.NamespaceName);
+                    Assert.AreSame(child, doc.XPathSelectElement("//x:elem/default:child", nsResolver));
+                });
+            });
+        }
+
+        [TestMethod]
+        public void NamespaceUndeclaredInTargetDocument()
+        {
+            var doc = Shared.GetTestSampleWithNamespaces();
+            var diff = XDocument.Load(@"TestData\A18_NamespaceMangling\NamespaceMangling_NamespaceNotDeclaredInTarget.xml");
+
+            var nsResolver = doc.GetNamespaceResolver("default");
+
+            Shared.Patcher.PatchXml(doc, diff);
+
+            var addedNode = doc.XPathSelectElement("//default:original", nsResolver)!.LastNode!;
             Assert.AreEqual(XmlNodeType.Element, addedNode.NodeType);
             Shared.TestWith((XElement)addedNode, elem =>
             {
                 Assert.AreEqual("elem", elem.Name.LocalName);
-                Assert.AreEqual("http://schemas.microsoft.com/winfx/2006/xaml", elem.Name.Namespace.NamespaceName);
-                Assert.AreSame(elem, doc.XPathSelectElement("//x:elem"));
-
-                Shared.TestWith((XElement)elem.FirstNode!, child =>
-                {
-                    Assert.AreEqual("child", child.Name.LocalName);
-                    Assert.AreEqual("http://www.w3.org/2001/XMLSchema", child.Name.Namespace.NamespaceName);
-                    Assert.AreSame(child, doc.XPathSelectElement("//x:elem/default:child"));
-                });
+                Assert.AreEqual("urn:ns:new-namespace", elem.Name.Namespace.NamespaceName);
             });
         }
     }
