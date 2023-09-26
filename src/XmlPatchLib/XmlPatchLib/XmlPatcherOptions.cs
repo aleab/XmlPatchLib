7a662a4c961ed6b6ad391d1afd2d7f6daf6c79ac
aleab <11708634+aleab@users.noreply.github.com>
2023-09-26 19:00:09 +0200

    ~

--- src/XmlPatchLib/XmlPatchLib/XmlPatcherOptions.cs
+++ src/XmlPatchLib/XmlPatchLib/XmlPatcherOptions.cs
@@ -2,8 +2,14 @@
 
 namespace Tizuby.XmlPatchLib
 {
+    /// <summary>
+    ///     Options to use with an <see cref="XmlPatcher"/>.<br/>
+    ///     These options allow to use a custom <see cref="IXPathEvaluator"/> or enable behaviours and features not normally allowed by the spec.
+    /// </summary>
     public class XmlPatcherOptions : IPatchOperationOptions
     {
+        public IXPathEvaluator XPathEvaluator { get; set; }
+
         /// <summary>
         ///     The xs:NCName of the patch document's root element.
         /// </summary>
@@ -14,8 +20,6 @@ namespace Tizuby.XmlPatchLib
         /// </summary>
         public bool UseBestEffort { get; set; }
 
-        public IXPathEvaluator XPathEvaluator { get; set; }
-
         /// <summary>
         ///     Whether to allow the 'sel' attribute to select multiple nodes or not.<br/><br/>
         ///     The default behaviour (as per section 4.1 of the RFC) does not allow multi-node selectors and throws an
@@ -25,8 +29,7 @@ namespace Tizuby.XmlPatchLib
 
         /// <summary>
         ///     Whether or not to disable the following restrictions on <c>&lt;replace&gt;</c> operations.<br/><br/>
-        ///     &#x2022; A <c>&lt;replace&gt;</c> operation MUST have exactly one node; it MAY be empty when replacing a text node.
-        ///     <br/>
+        ///     &#x2022; A <c>&lt;replace&gt;</c> operation MUST have exactly one node; it MAY be empty when replacing a text node.<br/>
         ///     &#x2022; A <c>&lt;replace&gt;</c> operation MUST replace a target node with a node of the same type.
         /// </summary>
         public bool DisableReplaceRestrictions { get; set; }
@@ -42,9 +45,10 @@ namespace Tizuby.XmlPatchLib
         {
             if (options != null)
             {
+                this.XPathEvaluator = options.XPathEvaluator;
                 this.RootElementName = options.RootElementName;
                 this.UseBestEffort = options.UseBestEffort;
-                this.XPathEvaluator = options.XPathEvaluator;
+
                 this.AllowMultiNodeSelectors = options.AllowMultiNodeSelectors;
                 this.DisableReplaceRestrictions = options.DisableReplaceRestrictions;
                 this.UseProcessingInstrutions = options.UseProcessingInstrutions;
